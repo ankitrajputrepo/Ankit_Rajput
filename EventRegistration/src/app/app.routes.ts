3aff7f0113ecd783c6bad505c36959cd952b74ab
Ankit R <ankit.r@thinkbridge.in>
2024-07-18 12:10:58 +0530

    added routes for all components

--- EventRegistration/src/app/app.routes.ts
+++ EventRegistration/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { EventDetailsComponent } from './components/event-details/event-details.component';
 import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
+import { SuccessfulPageComponent } from './components/successful-page/successful-page.component';
+import { AdminPageComponent } from './components/admin-page/admin-page.component';
 
 
 export const routes: Routes = [
@@ -16,6 +18,12 @@ export const routes: Routes = [
         path:'admin',component:LoginPageComponent
     },
     { path: 'eventdetails', component: EventDetailsComponent },
-    { path: 'registrationpage', component: RegistrationPageComponent }
+    { path: 'registrationpage', component: RegistrationPageComponent },
+    {
+        path:'success',component:SuccessfulPageComponent
+    },
+    {
+        path:'adminpage',component:AdminPageComponent
+    }
 
 ];
