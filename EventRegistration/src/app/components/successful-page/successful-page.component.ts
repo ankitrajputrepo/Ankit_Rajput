5c92d4ff6a78922a6d645db50bdbc194f5649687
Ankit R <ankit.r@thinkbridge.in>
2024-07-18 09:01:15 +0530

    added eventregistration form and setup environment

---
+++ EventRegistration/src/app/components/successful-page/successful-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-successful-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './successful-page.component.html',
+  styleUrl: './successful-page.component.css'
+})
+export class SuccessfulPageComponent {
+
+}
