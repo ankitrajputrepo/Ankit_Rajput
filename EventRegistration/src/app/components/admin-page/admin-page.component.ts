ce28b3c0f5c18046571bf2a55e65834892e0242f
Ankit R <ankit.r@thinkbridge.in>
2024-07-18 12:10:19 +0530

    admin page where admin can see the userlist

--- EventRegistration/src/app/components/admin-page/admin-page.component.ts
+++ EventRegistration/src/app/components/admin-page/admin-page.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [],
+  imports: [DatePipe,CommonModule],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.css'
 })
 export class AdminPageComponent {
+  userList: any[] = []; // Initialize an empty array to hold user data
 
+  ngOnInit(): void {
+    if (typeof localStorage !== 'undefined') {
+      // Safely access localStorage
+      const storedUsers = localStorage.getItem('registrations');
+      if (storedUsers) {
+        this.userList = JSON.parse(storedUsers);
+      }
+    } 
+  }
 }
