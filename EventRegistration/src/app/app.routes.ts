c546345ae5570f6c857bb2e74d2e5c7f9dcd36b3
Ankit R <ankit.r@thinkbridge.in>
2024-07-18 10:45:14 +0530

    defining routes of event details page and registration form page and also set default routes

--- EventRegistration/src/app/app.routes.ts
+++ EventRegistration/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { EventDetailsComponent } from './components/event-details/event-details.component';
+import { RegistrationPageComponent } from './components/registration-page/registration-page.component';
+
+
+export const routes: Routes = [
+
+    {
+        path:'',component:HomeComponent
+    }
+    ,
+    {
+        path:'admin',component:LoginPageComponent
+    },
+    { path: 'eventdetails', component: EventDetailsComponent },
+    { path: 'registrationpage', component: RegistrationPageComponent }
+
+];
